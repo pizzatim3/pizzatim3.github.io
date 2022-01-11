125da082a30de87aac30b6298fd5a265d0190a97
Ben Hanson <ben.hanson@xero.com>
2022-01-11 16:27:40 +1300

    adjust homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import pizza from './pizza.png';
 import './App.css';
 
+var currentCode;
+var animationDuration = 25;
+var logoHeight = 40
+
 function App() {
+Initialise()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            pizza time
+          </p>
+        <a onClick={OnClick}><img src={pizza} className="App-logo" alt="rotating pizza - click to copy the number to your clipboard"/></a>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          your number is {currentCode}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+function Initialise(){
+  
+  console.log("stop trying to hack my website -ben")
+  GenerateCode()
+  document.documentElement.style.setProperty('--animation-duration', animationDuration + 's');
+  document.documentElement.style.setProperty('--logo-height', logoHeight + 'vmin');
+}
+
+function GenerateCode() {
+  var min = 1667; //lowest 5 digit number divisible by 6 divided by 6
+  var max = 16666; //highest 5 digit number divisible by 6 divided by 6
+  currentCode = Math.floor(Math.random() * (max - min + 1) + min) * 6;
+}
+
+function OnClick() {
+  navigator.clipboard.writeText(currentCode)
+  animationDuration = animationDuration / 1.5;
+  if (animationDuration >= 0.0000059604644775390625) {
+    document.documentElement.style.setProperty('--animation-duration', animationDuration + 's');
+  } else {
+    window.open('https://www.youtube.com/watch?v=czTksCF6X8Y','_self');
+  }
+  logoHeight = logoHeight * 1.1
+  document.documentElement.style.setProperty('--logo-height', logoHeight + 'vmin');
 }
 
 export default App;
